81dae093c9bbc88aef4b3440dabe7106ace35e78
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-26 18:32:32 -0400

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Gabriel Marquez on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
